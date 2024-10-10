56218d85cc352834dad45b8a818e552c81a72c41
Vitaliy <163574647+vitalybell@users.noreply.github.com>
2024-10-10 15:33:47 +0300

    Update views.py

--- UrbanDjango/task2/views.py
+++ UrbanDjango/task2/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
 def show_task_2(requests):
-    return render(requests,  'second_task/func_template.html')
+    return render(requests,  'func_template.html')
+
+class show_task_class_2(TemplateView):
+    template_name = 'class_template.html'
